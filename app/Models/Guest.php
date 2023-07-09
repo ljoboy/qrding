46f6858df0fab1bb9c2f1de138a26bd6d4edfac5
Dark Angel <jonathanyombo@gmail.com>
2023-07-09 21:59:06 +0200

    softdeletes

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Guest extends Model
 {
     use HasFactory;
     use HasUuids;
+    use SoftDeletes;
 }
