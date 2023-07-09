3b71760bceee4539d5940378919edd9c550ecb3c
Dark Angel <jonathanyombo@gmail.com>
2023-07-09 22:42:15 +0200

    methods

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -2,12 +2,56 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Party extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'start_time',
+        'duration',
+    ];
+
+    protected array $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function guests(): HasMany
+    {
+        return $this->hasMany(Guest::class);
+    }
+
+    public function tables(): HasMany
+    {
+        return $this->hasMany(Table::class);
+    }
+
+    public function scopeCurrent($query)
+    {
+        return $query->where('start_time', '<=', now())->where('end_time', '>=', now());
+    }
+
+    public function scopeFuture($query)
+    {
+        return $query->where('start_time', '>', now());
+    }
+
+    public function scopePast($query)
+    {
+        return $query->whereNotNull('end_time')->where('end_time', '<', now());
+    }
+
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
