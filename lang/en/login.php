216d7c42b854617cba3514bf08b3110f5ba48eaf
Dark Angel <jonathanyombo@gmail.com>
2023-07-10 13:55:27 +0200

    login lang

---
+++ lang/en/login.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'login' => 'Login',
+    'qrding' => [
+        'logo_text' => 'QRding',
+        'intro_text' => 'A few more clicks to <br> sign in to your account.',
+        'intro_text2' => 'Manage all your e-commerce accounts in one place.',
+        'sign_in_heading' => 'Sign In',
+        'email_placeholder' => 'midone@left4code.com',
+        'password_placeholder' => 'Password',
+        'remember_me' => 'Remember me',
+        'forgot_password' => 'Forgot Password?',
+        'login_button' => 'Login',
+        'register_button' => 'Register',
+        'agree' => 'By signing up, you agree to our',
+        'terms_conditions' => 'Terms and Conditions',
+        'privacy_policy' => 'Privacy Policy',
+    ],
+];
---
+++ lang/fr/login.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'login' => 'Connexion',
+    'qrding' => [
+        'logo_text' => 'QRding',
+        'intro_text' => 'Quelques clics pour <br> vous connecter à votre compte.',
+        'intro_text2' => 'Gérez tous vos comptes de commerce électronique en un seul endroit.',
+        'sign_in_heading' => 'Se connecter',
+        'email_placeholder' => 'midone@left4code.com',
+        'password_placeholder' => 'Mot de passe',
+        'remember_me' => 'Se souvenir de moi',
+        'forgot_password' => 'Mot de passe oublié ?',
+        'login_button' => 'Connexion',
+        'register_button' => 'S\'inscrire',
+        'agree' => 'En vous inscrivant, vous acceptez nos',
+        'terms_conditions' => 'Conditions générales',
+        'privacy_policy' => 'Politique de confidentialité',
+    ],
+];
