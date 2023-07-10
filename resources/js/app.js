c527504f1ca0b60a9a725ea1e357b75e2e1d6a43
Dark Angel <jonathanyombo@gmail.com>
2023-07-10 05:14:17 +0200

    js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,12 +7,12 @@
  |
  */
 import "./bootstrap";
-// import "@left4code/tw-starter/dist/js/svg-loader";
-// import "@left4code/tw-starter/dist/js/accordion";
-// import "@left4code/tw-starter/dist/js/alert";
-// import "@left4code/tw-starter/dist/js/dropdown";
-// import "@left4code/tw-starter/dist/js/modal";
-// import "@left4code/tw-starter/dist/js/tab";
+import "@left4code/tw-starter/dist/js/svg-loader";
+import "@left4code/tw-starter/dist/js/accordion";
+import "@left4code/tw-starter/dist/js/alert";
+import "@left4code/tw-starter/dist/js/dropdown";
+import "@left4code/tw-starter/dist/js/modal";
+import "@left4code/tw-starter/dist/js/tab";
 
 /*
  |--------------------------------------------------------------------------
--- resources/js/helper.js
+++ resources/js/helper.js
@@ -123,7 +123,7 @@ const helpers = {
         const rgbColors = Object.entries(tempColors);
         for (const [key, value] of rgbColors) {
             if (typeof value === "string") {
-                if (value.replace("#", "").length == 6) {
+                if (value.replace("#", "").length === 6) {
                     const aRgbHex = value.replace("#", "").match(/.{1,2}/g);
                     tempColors[key] = (opacity = 1) =>
                         `rgb(${parseInt(aRgbHex[0], 16)} ${parseInt(
