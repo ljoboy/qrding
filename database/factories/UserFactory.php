0541796cfca1f6e8b1ea8e4852c49a7b720bca60
Dark Angel <jonathanyombo@gmail.com>
2023-07-11 21:58:34 +0200

    add phone

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Enums\RoleEnum;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
@@ -21,8 +23,10 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password', // password
             'remember_token' => Str::random(10),
+            'role' => fake()->randomElement(RoleEnum::getValues()),
+            'phone' => fake()->E164PhoneNumber(),
         ];
     }
 
@@ -31,7 +35,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
