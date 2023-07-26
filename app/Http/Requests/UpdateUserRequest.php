dae07be2eb8c94effd01442249f29719fb185fd7
Dark Angel <jonathanyombo@gmail.com>
2023-07-26 13:21:01 +0200

    imrpovement

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateUsersRequest extends FormRequest
+final class UpdateUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,7 +24,7 @@ class UpdateUsersRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+
         ];
     }
 }
